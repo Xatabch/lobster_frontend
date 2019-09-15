adaad1b26ecac95fe8da86592122c65bd1927e4a
Ivan <ivan.morozov@list.ru>
2019-09-15 06:05:00 +0300

    refactoring

--- src/components/AddPost/AddPost.js
+++ src/components/AddPost/AddPost.js
@@ -139,10 +139,14 @@ export default class AddPost extends Component {
                             <div className="addpost__attach">
                                 <button className={classnames('attach__button', 
                                                    this.state.attachOpen ? 'attach__button_theme_open' : '')}
-                                        onClick={() => this.onOpenButtonClick()}>+</button>
+                                        onClick={() => this.onOpenButtonClick()}>
+                                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <path d="M12 5V19" stroke="#FFFEFE" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+                                        <path d="M5 12H19" stroke="#FFFEFE" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+                                    </svg>
+                                </button>
                             <div className={classnames('attach__photo', 
-                                                       this.state.attachOpen  ? '' : 'attach_theme_hidden', 
-                                                       this.state.attachOpen  ? 'attach__photo_theme_open' : '')}>
+                                                       this.state.attachOpen ? 'attach__photo_theme_open' : 'attach_theme_hidden')}>
                                 <button className="photo__button">
                                     <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                                         <path d="M23 19C23 19.5304 22.7893 20.0391 22.4142 20.4142C22.0391 20.7893 21.5304 21 21 21H3C2.46957 21 1.96086 20.7893 1.58579 20.4142C1.21071 20.0391 1 19.5304 1 19V8C1 7.46957 1.21071 6.96086 1.58579 6.58579C1.96086 6.21071 2.46957 6 3 6H7L9 3H15L17 6H21C21.5304 6 22.0391 6.21071 22.4142 6.58579C22.7893 6.96086 23 7.46957 23 8V19Z" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
